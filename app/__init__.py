fd01616ea1c810b35078f1466f930146139acf2f
unknown <you@example.com>
2024-03-15 23:33:59 +0200

    req

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,17 @@ import os
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_cors import CORS
+
 db = SQLAlchemy()
 
 #FROM https://github.com/miguelgrinberg/microblog/blob/v0.15/app/__init__.py	
 def create_app(config_class=Config):
     app = Flask(__name__)        
     app.config.from_object(config_class)        
-    db.init_app(app)	
+    db.init_app(app)
+    CORS().init_app(app, resources={r"/submit_order": {"origins": "http://localhost:3000/"}})
+	
     return app
 
 from app import models
