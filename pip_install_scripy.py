748c28415fd3cc55b6f914d8b2931716dec2bc9e
unknown <you@example.com>
2024-03-16 03:42:15 +0200

    order

---
+++ pip_install_scripy.py
@@ -0,0 +1,16 @@
+python -m venv venv
+venv\Scripts\activate
+pip install -r requirements.txt
+
+pip install flask
+
+pip install flask_sqlalchemy;
+pip install jsonify
+pip install psycopg2;
+pip install gunicorn;
+pip install psycopg2-binary;
+pip install requests;
+
+set FLASK_ENV=development
+set FLASK_DEBUG=1
+
